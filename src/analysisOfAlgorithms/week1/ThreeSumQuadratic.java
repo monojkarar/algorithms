3ac5874ddad2fc200f689855a8b3421457b0e0b3
Raul Zuniga <rzuniga64@gmail.com>
2016-12-16 01:36:27 -0600

    Update syntax.

--- src/analysisOfAlgorithms/week1/ThreeSumQuadratic.java
+++ src/analysisOfAlgorithms/week1/ThreeSumQuadratic.java
@@ -16,12 +16,16 @@ import java.util.Scanner;
  * in the worst case. You may assume that you can sort the N integers in time
  * proportional to N^2 or better.
  * <p>
- * Hint: given an integer x and a sorted array a[] of n distinct integers, design a linear-time algorithm to determine
+ * Hint: given an integer x and a sorted array a[] of n distinct integers,
+ * design a linear-time algorithm to determine
  * if there exists two distinct indices i and j such that a[i]+a[j]==x.
  */
 public class ThreeSumQuadratic {
 
-    private static void findThreeSum(ArrayList<Integer> a) {
+    /**
+     * findThreeSum.
+     */
+    private static void findThreeSum(final ArrayList<Integer> a) {
 
         // Compute all 3-sum combinations
         for (int i = 0; i < a.size() - 2; ++i) {
@@ -31,7 +35,8 @@ public class ThreeSumQuadratic {
             while (j < k) {
                 int sum = a.get(i) + a.get(j) + a.get(k);
                 if (sum == 0) {
-                    System.out.println(a.get(i) + " " + a.get(j) + " " + a.get(k));
+                    System.out.println(a.get(i) + " " + a.get(j) + " "
+                            + a.get(k));
                 }
                 if (sum >= 0) {
                     --k;
@@ -42,7 +47,12 @@ public class ThreeSumQuadratic {
         }
     }
 
-    public static void main(String[] args) {
+    /**
+     * Main.
+     *
+     * @param args the args
+     */
+    public static void main(final String[] args) {
         //initialize array
         ArrayList<Integer> a = new ArrayList<>();
 
