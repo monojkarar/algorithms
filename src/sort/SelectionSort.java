f1c4f97a7c9a194267cf299ad66b2d022bed0fdd
Raul Zuniga <rzuniga64@gmail.com>
2016-12-16 02:01:48 -0600

    Update syntax.

--- src/sort/SelectionSort.java
+++ src/sort/SelectionSort.java
@@ -1,57 +1,70 @@
 package sort;
 
+
 import static sort.SortUtility.generateRandomArray;
 import static sort.SortUtility.printHorizontalArray;
 
 /**
- * 	Selection Sort is normally the best of the elementary sorts.
- *	There is a pass for each position (0..size-1)
- *	On each pass, the smallest (minimum) element in the rest of the list is exchanged (swapped) with element at the
- *	current position. The first part of the list (already processed) is always sorted. Each pass increases the size
- *	of the sorted portion
+ *  Selection Sort is normally the best of the elementary sorts.
+ *  There is a pass for each position (0..size-1).
+ *  On each pass, the smallest (minimum) element in the rest of the list is
+ *  exchanged (swapped) with element at the current position. The first part
+ *  of the list (already processed) is always sorted. Each pass increases the
+ *  size of the sorted portion.
  *
- * 	Runtime analysis
- * 	Best    Average     Worst
- * 	O(n*n)  O(n*n)      O(n*n)
+ *  Runtime analysis
+ *  Best    Average     Worst
+ *  O(n*n)  O(n*n)      O(n*n)
  *
- *	Array Position	0 	1	2	3	4
- *	Initial State	36	24	10	6	12
- *	After 1st pass	6 	24	10	36	12	6 and 36 are swapped
- *	After 2nd pass	6 	10 	24	36	12	10 and 24 are swapped
- *	After 3rd pass	6	10	12	36	24	12 and 24 are swapped
- *	After 4th pass	6 	10	12	24	36	24 and 36 are swapped
+ *  Array Position  0   1   2   3   4
+ *  Initial State  36   24  10   6  12
+ *  After 1st pass  6   24  10  36  12   6 and 36 are swapped.
+ *  After 2nd pass  6   10  24  36  12  10 and 24 are swapped.
+ *  After 3rd pass  6   10  12  36  24  12 and 24 are swapped.
+ *  After 4th pass  6   10  12  24  36  24 and 36 are swapped.
  *
- *	N is the number of elements in the list
- *	Outer loop executes N-1 times
- *	Inner loop executes N-1, then N-2, then N-3, ... then once.
- *	Total number of comparisons (in inner loop) is the sum of 1 to N-1 is N(N + 1) /2
- *	Efficiency of algorith is O(N2)
+ *  N is the number of elements in the list.
+ *  Outer loop executes N-1 times.
+ *  Inner loop executes N-1, then N-2, then N-3, ... then once.
+ *  Total number of comparisons (in inner loop) is the sum of 1 to N-1 is
+ *  N(N + 1) /2.
+ *  Efficiency of algorith is O(N2).
  */
 
 public class SelectionSort {
 
+    /** The array to sort. */
     private int[] theArray = new int[50];
+    /** The size of the array. */
     private int arraySize = 10;
 
-    private void swapValues(int indexOne, int indexTwo){
+    /**
+     * SwapValues.
+     * @param indexOne the indexOne
+     * @param indexTwo the index Two
+     */
+    private void swapValues(final int indexOne, final int indexTwo) {
 
         int temp = theArray[indexOne];
         theArray[indexOne] = theArray[indexTwo];
         theArray[indexTwo] = temp;
     }
 
-    /** On each pass, the smallest (minimum) element in the rest of the list is exchanged (swapped) with element at the
-     *	current position. and then repeats searching through the entire array each time */
+    /**
+     * On each pass, the smallest (minimum) element in the rest of the list is
+     * exchanged (swapped) with element at the current position. and then
+     * repeats searching through the entire array each time */
 
-    private void selectionSort(){
+    private void selectionSort() {
 
-        for(int x=0; x < arraySize; x++){
+        for (int x = 0; x < arraySize; x++) {
             int minimum = x;
 
-            for(int y=x; y < arraySize; y++){
+            for (int y = x; y < arraySize; y++) {
                 // To change direction of sort just change this from > to <
-                if( theArray[y] < theArray[minimum])
+                if (theArray[y] < theArray[minimum]) {
                     minimum = y;
+                }
             }
 
             swapValues(x, minimum);
@@ -59,11 +72,15 @@ public class SelectionSort {
         }
     }
 
-    public static void main(String[] args){
+    /**
+     * The entry point.
+     * @param args the args
+     */
+    public static void main(final String[] args) {
 
         SelectionSort newArray = new SelectionSort();
         generateRandomArray(newArray.theArray, newArray.arraySize);
-        printHorizontalArray(newArray.theArray, newArray.arraySize, -1,-1);
+        printHorizontalArray(newArray.theArray, newArray.arraySize, -1, -1);
         newArray.selectionSort();
     }
 }
