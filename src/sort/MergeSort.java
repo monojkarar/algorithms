c493e1b97bc5b5d6cd602542304fee9dcb818563
Raul Zuniga <rzuniga64@gmail.com>
2016-12-19 00:02:44 -0600

    Update MergeSort.

--- src/sort/MergeSort.java
+++ src/sort/MergeSort.java
@@ -4,9 +4,27 @@ import static sort.SortUtility.generateRandomArray;
 import static sort.SortUtility.printHorizontalArray;
 
 /**
- * The type Merge sort.
+ *  The MergeSort.
+ *  MergeSort is a divide and conquer algorithm that is a  an efficient,
+ *  general-purpose, comparison-based sorting algorithm
+ *
+ *  Proposition: MergeSort uses at most N lg N compares and 6 N lg N array
+ *  accesses to sort any array of size N. Extra cost of
+ *  merge =2^K * (N/2^K) lg N (# of stages).
+ *
+ *  Runtime analysis
+ *  Best        Average     Worst
+ *  O(n log n)  O(n log n)  O(n log n)
+ *
+ *  Worst-case space complexity O(n) total, O(n) auxiliary
+ *
+ *  Upper bound is an algorithm guaranteed to get the sort done in time
+ *  proportional to worst case running time.
  */
 public final class MergeSort {
+    /** A variable to determine when number of items to sort is small. */
+    private  final static int CUTOFF = 7;
+
     /** The array to sort. */
     private Comparable[] theArray;
     /**
@@ -27,10 +45,20 @@ public final class MergeSort {
     private void mergesort(final Comparable[] array, final Comparable[] aux,
                            final int low, final int high) {
 
+        // performance improvement
+        if (high <= low + CUTOFF - 1) {
+            InsertionSort.insertionSort(array);
+            return;
+        }
+
         int middle = (low + high) / 2;
+
         mergesort(array, low, middle);
         mergesort(array, middle + 1, high);
-
+        // Is biggest item in 1st half <= samllest item in 2nd half. If so stop.
+        if (!less(array[middle + 1], array[middle])) {
+            return;
+        }
         // copy to aux[]
         for (int k = low; k <= high; k++) {
             aux[k] = array[k];
@@ -45,7 +73,7 @@ public final class MergeSort {
             else                           array[k] = aux[i++];
         }
 
-        assert (isSorted(array));
+        //assert (isSorted(array, low, high));
     }
 
     /**
@@ -80,11 +108,14 @@ public final class MergeSort {
      *  than its predecessor. If we reach the end of the loop without finding
      *  that the array is unsorted, then it must be sorted instead.
      *  @param a the array to check.
+     *  @param start the start.
+     *  @param end the end.
      *  @return true if sorted; false otherwise.
      */
-    private boolean isSorted(final Comparable[] a) {
+    private boolean isSorted(final Comparable[] a, final int start,
+                             final int end) {
 
-        for (int i = 0; i < a.length - 1; i++) {
+        for (int i = start; i < end - 1; i++) {
             if (less(a[i + 1], a[i])) {
                 return false; // It is proven that the array is not sorted.
             }
@@ -105,18 +136,16 @@ public final class MergeSort {
 
         MergeSort ms = new MergeSort(Integer.parseInt(args[0]));
 
-        System.out.println("STARTING ARRAY\n");
+        System.out.println("STARTING ARRAY");
         printHorizontalArray(ms.theArray, ms.theArray.length, -1, -1);
-        System.out.println();
 
         startTime = System.currentTimeMillis();
         // Send the array, 0 and the array size
         ms.mergesort(ms.theArray, 0, ms.theArray.length - 1);
         endTime = System.currentTimeMillis();
 
-        System.out.print("FINAL SORTED ARRAY\n");
+        System.out.println("FINAL SORTED ARRAY");
         printHorizontalArray(ms.theArray, ms.theArray.length, -1, -1);
-        System.out.println();
         System.out.println("Mergesort took " + (endTime - startTime)
                 + " milliseconds.");
     }
