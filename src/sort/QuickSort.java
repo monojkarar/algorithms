d9b9f55d6a124ac375ea743e9eb85ed753ac2145
Raul Zuniga <rzuniga64@gmail.com>
2016-12-22 01:34:16 -0600

    Implement quick sort algorithm.

--- src/sort/QuickSort.java
+++ src/sort/QuickSort.java
@@ -1,152 +1,148 @@
 package sort;
 
+import edu.princeton.cs.algs4.StdRandom;
+
 import java.util.Arrays;
+
+import static sort.SortUtility.generateRandomArray;
 
 /**
  * In most situations the sort.QuickSort is the fastest sorting algorithm.
- * The sort.QuickSort works by partioning arrays that the smaller numbers
+ * The sort.QuickSort works by partitioning arrays that the smaller numbers
  * are on the left and the larger are on the right.  It then recursively
  * sends small parts of larger arrays to itself and partitions again.
  *
+ * Basic plan.
+ * - Shuffle the array.
+ * - Partition so that, for some j
+ *   - entry[j] is in place
+ *   - no larger entry to the left of j
+ *   - no smaller entry to the right of j
+ * - Sort each piece recursively.
  */
-public class QuickSort {
-
-    private static int[] theArray;
-    private static int arraySize;
-
-    public static void main(String[] args) {
-        QuickSort theSort = new QuickSort(10);
-        theSort.generateRandomArray();
-        System.out.println(Arrays.toString(QuickSort.theArray));
-        theSort.quickSort(0, 9);
-        System.out.println(Arrays.toString(QuickSort.theArray));
+public final class QuickSort {
+    /** The array to sort. */
+    private  Comparable[] theArray;
+    /**
+     *  The constructor.
+     *  @param n the number of items in array.
+     */
+    private QuickSort(final int n) {
+        theArray = new Comparable[n];
+        generateRandomArray(this.theArray, this.theArray.length);
     }
 
-    private QuickSort(int newArraySize) {
-
-        arraySize = newArraySize;
-        theArray = new int[arraySize];
-        generateRandomArray();
+    /**
+     *  The Quicksort algorithm.
+     *  @param array the array to sort
+     */
+    private static void quickSort(final Comparable[] array) {
+        StdRandom.shuffle(array);
+        quickSort(array, 0, array.length - 1);
     }
+    /**
+     * @param array the array to sort
+     * @param left the left
+     * @param right the right
+     */
+    private static void quickSort(final Comparable[] array, final int left,
+                      final int right) {
 
-    private void quickSort(int left, int right) {
-
-        if (right - left <= 0)
-            return; // Everything is sorted
-        else {
-            // It doesn't matter what the pivot is, but it must be a value in the array
-            int pivot = theArray[right];
-            System.out.println("Value in right " + theArray[right]
-                    + " is made the pivot");
-
-            System.out.println("left = " + left + " right= " + right
-                    + " pivot= " + pivot + " sent to be partitioned");
-
-            int pivotLocation = partitionArray(left, right, pivot);
-            System.out.println("Value in left " + theArray[left]
-                    + " is made the pivot");
-
-            quickSort(left, pivotLocation - 1); // Sorts the left side
-            quickSort(pivotLocation + 1, right);
+        if (right <= left) {
+            return;         // Everything is sorted
         }
+        int j = partition(array, left, right);
+
+        quickSort(array, left, j - 1); // Sorts the left side
+        quickSort(array, j + 1, right);
     }
 
-    private int partitionArray(int left, int right, int pivot) {
+    /**
+     * Phase 1: Repeat until leftPtr and rightPtr cross.
+     * - Scan leftPtr from left to right so long as (a[leftPtr] < a[lo]).
+     * - Scan rightPtr from right to left so long as (a[rightPtr] > a[lo]).
+     * - Exchange a[i] with a[j].
+     *
+     * Phase 2: When pointers cross.
+     * - Exchange a[lo] with a[j].
+     *
+     * @param array the array to sort
+     * @param left the left pointer
+     * @param right the right pointer
+     * @return the left pointer
+     */
+    private static int partition(final Comparable[] array, final int left,
+                               final int right) {
 
-        int leftPointer = left - 1;
-        int rightPointer = right;
+        int i = left;
+        int j = right + 1;
 
+        // find item on left to swap
         while (true) {
-            while (theArray[++leftPointer] < pivot) ;
-
-            printHorzArray(leftPointer, rightPointer);
-            System.out.println(theArray[leftPointer] + " in index "
-                    + leftPointer + " is bigger than the pivot value " + pivot);
-
-            while (rightPointer > 0 && theArray[--rightPointer] > pivot) ;
-
-            printHorzArray(leftPointer, rightPointer);
-            System.out.println(theArray[rightPointer] + " in index "
-                    + rightPointer + " is smaller than the pivot value "
-                    + pivot);
-
-            printHorzArray(leftPointer, rightPointer);
-
-            if (leftPointer >= rightPointer) {
-
-                System.out.println("left is >= right so start again");
-                break;
-            } else {
-                swapValues(leftPointer, rightPointer);
-                System.out.println(theArray[leftPointer] + " was swapped for "
-                        + theArray[rightPointer]);
+            while (less(array[++i], array[left])) {
+                if (i == right) {
+                    break;
+                }
             }
+            // find item on right to swap
+            while (less(array[left], array[--j])) {
+                if (j == left) {
+                    break;
+                }
+            }
+
+            if (i >= j) {       // check if pointers cross.
+                break;
+            }
+            exch(array, i, j);  // swap.
         }
 
-        swapValues(leftPointer, right);
-        return leftPointer;
+        exch(array, left, j);   // swap with partitioning item.
+        return j;               // return index of item known to be in place.
     }
 
-    private void swapValues(int indexOne, int indexTwo) {
-
-        int temp = theArray[indexOne];
-        theArray[indexOne] = theArray[indexTwo];
-        theArray[indexTwo] = temp;
+    /**
+     * Return true if v < w; false otherwise.
+     * @param v the variable v
+     * @param w tje variable y
+     * @return boolean true if v < y; false otherwise.
+     */
+    private static boolean less(final Comparable v, final Comparable w) {
+        return v.compareTo(w) < 0;
     }
 
-    private void generateRandomArray() {
+    /**
+     * SwapValues.
+     * @param array the array
+     * @param indexOne the indexOne
+     * @param indexTwo the index Two
+     */
+    private static void exch(final Comparable[] array, final int indexOne, final
+    int indexTwo) {
 
-        for (int i = 0; i < arraySize; i++) {
-            // Generate a random array with values between 10 and 59
-            theArray[i] = (int) (Math.random() * 50) + 10;
-        }
+        Comparable temp = array[indexOne];
+        array[indexOne] = array[indexTwo];
+        array[indexTwo] = temp;
     }
 
-    private static void printHorzArray(int i, int j) {
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(final String[] args) {
 
-        for (int n = 0; n < 61; n++)
-            System.out.print("-");
+        long startTime;
+        long endTime;
 
+        QuickSort theSort = new QuickSort(Integer.parseInt(args[0]));
+        System.out.println(Arrays.toString(theSort.theArray));
+        startTime = System.currentTimeMillis();
+        QuickSort.quickSort(theSort.theArray);
+        endTime = System.currentTimeMillis();
+        System.out.println(Arrays.toString(theSort.theArray));
         System.out.println();
-
-        for (int n = 0; n < arraySize; n++)
-            System.out.format("| %2s " + " ", n);
-
-        System.out.println("|");
-
-        for (int n = 0; n < 61; n++)
-            System.out.print("-");
-
-        System.out.println();
-
-        for (int n = 0; n < arraySize; n++)
-            System.out.print(String.format("| %2s " + " ", theArray[n]));
-
-        System.out.println("|");
-
-        for (int n = 0; n < 61; n++)
-            System.out.print("-");
-
-        System.out.println();
-
-        if (i != -1) {
-
-            // Number of spaces to put before the F
-            int spacesBeforeFront = 6 * (i + 1) - 5;
-
-            for (int k = 0; k < spacesBeforeFront; k++)
-                System.out.print(" ");
-
-            System.out.print("L" + i);
-
-            // Number of spaces to put before the R
-            int spacesBeforeRear = 5 * (j + 1) - spacesBeforeFront;
-
-            for (int l = 0; l < spacesBeforeRear; l++)
-                System.out.print(" ");
-
-            System.out.print("R" + j);
-            System.out.println("\n");
-        }
+        System.out.println("Quicksort took " + (endTime - startTime)
+                + " milliseconds.");
     }
 }
