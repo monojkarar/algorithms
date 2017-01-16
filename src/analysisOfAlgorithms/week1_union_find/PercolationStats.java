0c9360d8b22980fa886a9048b0b7f6bf8a73ba53
Raul Zuniga <rzuniga64@gmail.com>
2017-01-16 02:12:27 -0600

    Update

--- src/analysisOfAlgorithms/week1_union_find/PercolationStats.java
+++ src/analysisOfAlgorithms/week1_union_find/PercolationStats.java
@@ -10,49 +10,55 @@ import edu.princeton.cs.algs4.Stopwatch;
  */
 public class PercolationStats {
 
-    private int N;
+    /** Number of rows and columns. */
+    private int n;
+    /** Number of trials. */
     private int trials;
+    /** The threshold. */
     private double[] threshold;
+    /** Open site count. */
     private int openSiteCount;
 
     /**
      * Perform trials independent experiments on an n-by-n grid.
      *
-     * @param n      size of an NxN grid
-     * @param trials the number of trials
+     * @param newN      size of an NxN grid
+     * @param newTrials the number of trials
      */
-    public PercolationStats(final int n, final int trials) {
+    public PercolationStats(final int newN, final int newTrials) {
 
-        if (n <= 0 || trials <= 0) {
+        if (newN <= 0 || newTrials <= 0) {
             throw new IllegalArgumentException("Size of row/column and number"
                     + "of trials must be larger than 0");
         }
-        this.N = n;
-        this.trials = trials;
-        this.threshold = new double[trials];
-        this.openSiteCount = 0;
+        n = newN;
+        trials = newTrials;
+        threshold = new double[trials];
+        openSiteCount = 0;
 
-        runTrials(N, trials);
+        runTrials(n, trials);
     }
 
     /**
      * Perform independent computational experiment for a number of trials on
      * an n-by-n grid.
+     * @param n the number of sites
+     * @param trials the number of trials
      */
-    private void runTrials(final int N, final int trials) {
+    private void runTrials(final int n, final int trials) {
         for (int i = 0; i < trials; i++) {
-            Percolation percolation = new Percolation(N);
+            Percolation percolation = new Percolation(n);
 
             while (!percolation.percolates()) {
-                int row = StdRandom.uniform(1, N + 1);
-                int col = StdRandom.uniform(1, N + 1);
+                int row = StdRandom.uniform(1, n + 1);
+                int col = StdRandom.uniform(1, n + 1);
 
                 if (!percolation.isOpen(row, col)) {
                     percolation.open(row, col);
                     openSiteCount++;
                 }
             }
-            threshold[i] = (double) openSiteCount / (N * N);
+            threshold[i] = (double) openSiteCount / (n * n);
             openSiteCount = 0;
         }
     }
@@ -102,13 +108,15 @@ public class PercolationStats {
      *
      * @param args the input arguments
      */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
 
-        PercolationStats perStats = new PercolationStats(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
+        PercolationStats perStats = new PercolationStats(
+                Integer.parseInt(args[0]), Integer.parseInt(args[1]));
         Stopwatch time = new Stopwatch();
         StdOut.printf("mean                     = %f\n", perStats.mean());
         StdOut.printf("stddev                   = %f\n", perStats.stddev());
-        StdOut.printf("95%% confidence Interval = %f, %f\n", perStats.confidenceLo(), perStats.confidenceHi());
+        StdOut.printf("95%% confidence Interval = %f, %f\n",
+                perStats.confidenceLo(), perStats.confidenceHi());
         StdOut.printf("Elapsed time             = " + time.elapsedTime());
     }
 }
