9bc36e421d74375091a5470b6fe81115daf48567
Raul Zuniga <rzuniga64@gmail.com>
2017-01-16 02:04:40 -0600

    Update

--- src/analysisOfAlgorithms/week1_union_find/ThreeSumQuadratic.java
+++ src/analysisOfAlgorithms/week1_union_find/ThreeSumQuadratic.java
@@ -9,21 +9,23 @@ import java.util.Scanner;
 /**
  * Coursera - Algorithms Part I
  * Week 1 - Interview Questions - Analysis of Algorithms
- * <p>
+ *
  * Question 1: 3-SUM in quadratic time
- * <p>
+ *
  * Design an algorithm for the 3-SUM problem that takes time proportional to N^2
  * in the worst case. You may assume that you can sort the N integers in time
  * proportional to N^2 or better.
- * <p>
+ *
  * Hint: given an integer x and a sorted array a[] of n distinct integers,
  * design a linear-time algorithm to determine
  * if there exists two distinct indices i and j such that a[i]+a[j]==x.
  */
-public class ThreeSumQuadratic {
+public final class ThreeSumQuadratic {
 
+    /** Constructor. */
+    private ThreeSumQuadratic() { }
     /**
-     * findThreeSum
+     * findThreeSum.
      * @param a the array
      */
     private static void findThreeSum(final ArrayList<Integer> a) {
