0fe7d87de846388d1625fd09a99a26935e9c4305
Raul Zuniga <rzuniga64@gmail.com>
2017-01-18 00:55:13 -0600

    Update

--- src/sort/KnuthShuffle.java
+++ src/sort/KnuthShuffle.java
@@ -40,12 +40,14 @@ public final class KnuthShuffle {
      * Rearranges an array of objects in uniformly random order
      * (under the assumption that Math.random() generates independent
      * and uniformly distributed numbers between 0 and 1).
+     * Common bug: r betwwen 0 anbd N-1.
+     * Correct variant: r between i and N-1.
      * @param array the array to be shuffled
      */
     private static void shuffle(final Object[] array) {
-        int size = array.length;
-        for (int i = 0; i < size; i++) {
-            int r = i + (int) (Math.random() * (size - i));
+        int n = array.length;
+        for (int i = 0; i < n; i++) {
+            int r = i + (int) (Math.random() * (n - i));
             Object swap = array[r];
             array[r] = array[i];
             array[i] = swap;
